f87cfa2c02fb390460be9c374a49b28edb77abfc
EGCM <euclid0888@gmail.com>
2022-07-19 22:18:19 +0800

    'create' part of the crud app

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
-    require_once 'php/init.php';
+require_once 'php/init.php';
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,6 +18,21 @@
     <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">TODO LIST</a>
     </nav>
+    <div class="container mt-5">
+      <?php
+      insertTask();
+      ?>
+      <form action="" method="GET">
+        <div class="row">
+          <div class="col-md-9 form-group">
+            <input class="form-control" type="text" name="items" id="" placeholder="Insert a new task" required>
+          </div>
+          <div class="col-md">
+            <input class="btn btn-success" type="submit" value="Add Task">
+          </div>
+        </div>
+      </form>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
---
+++ php/functions.php
@@ -0,0 +1,22 @@
+<?php
+function insertTask(){
+    if (!empty($_GET['items'])) {
+        $insert = new insert($_GET['items']);
+        if ($insert->insertTask()) {
+          echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole!</strong> You have inserted task successfully.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>';
+        } else {
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole!</strong> Insert task error.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>';
+        }
+    }
+}
+?>
