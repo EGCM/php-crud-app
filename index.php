99658776c60ccd1ded89c29668949e85215e05ee
EGCM <euclid0888@gmail.com>
2022-07-21 17:50:41 +0800

    Part 3 * add tables to view pending and completed task

--- index.php
+++ index.php
@@ -32,6 +32,12 @@ require_once 'php/init.php';
           </div>
         </div>
       </form>
+
+      <?php
+      $view = new view();
+      $view->viewData();
+      $view->viewCompletedData();
+      ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
